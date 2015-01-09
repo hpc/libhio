98908ef40e7bf9deeb93328e6209f43c08fb92db
Nathan Hjelm <hjelmn@lanl.gov>
2015-01-09 09:24:14 -0700

    Correct example program

--- src/test.c
+++ src/test.c
@@ -27,30 +27,28 @@ int main (int argc, char *argv[]) {
   }
 
   rc = hio_open (dataset, &element, "data", 0);
-  if (HIO_SUCCESS != rc) {
-      fprintf (stderr, "Could not create dataset element. reason: %d\n", rc);
+  if (HIO_SUCCESS == rc) {
+    rc = hio_write (element, 0, 0, data, 10, sizeof (int));
+    if (0 > rc) {
+      fprintf (stderr, "Could not write data to offset 0. reason: %d\n", rc);
+    }
+
+    rc = hio_write (element, 40, 0, data, 10, sizeof (int));
+    if (0 > rc) {
+      fprintf (stderr, "Could not write data to offset 0. reason: %d\n", rc);
+    }
+
+    rc = hio_write (element, 1000, 0, data2, 10, sizeof (int));
+    if (0 > rc) {
+      fprintf (stderr, "Could not write data to offset 0. reason: %d\n", rc);
+    }
+
+    rc = hio_close (&element);
+    if (HIO_SUCCESS != rc) {
+      fprintf (stderr, "Error closing element. reason: %d\n", rc);
+    }
   } else {
-      hio_close (&element);
-  }
-
-  rc = hio_write (element, 0, 0, data, 10, sizeof (int));
-  if (0 > rc) {
-    fprintf (stderr, "Could not write data to offset 0. reason: %d\n", rc);
-  }
-
-  rc = hio_write (element, 40, 0, data, 10, sizeof (int));
-  if (0 > rc) {
-    fprintf (stderr, "Could not write data to offset 0. reason: %d\n", rc);
-  }
-
-  rc = hio_write (element, 1000, 0, data2, 10, sizeof (int));
-  if (0 > rc) {
-    fprintf (stderr, "Could not write data to offset 0. reason: %d\n", rc);
-  }
-
-  rc = hio_close (&element);
-  if (HIO_SUCCESS != rc) {
-    fprintf (stderr, "Error closing element. reason: %d\n", rc);
+      fprintf (stderr, "Could not create dataset element. reason: %d\n", rc);
   }
 
   rc = hio_dataset_close (&dataset);
@@ -67,24 +65,22 @@ int main (int argc, char *argv[]) {
   }
 
   rc = hio_open (dataset, &element, "data", 0);
-  if (HIO_SUCCESS != rc) {
-      fprintf (stderr, "Could not create dataset element. reason: %d\n", rc);
-  } else {
-      hio_close (&element);
-  }
+  if (HIO_SUCCESS == rc) {
+    rc = hio_read (element, 1028, 0, data_read, 5, sizeof (int));
+    if (0 < rc) {
+      for (int i = 0 ; i < rc/sizeof(int) ; ++i) {
+	fprintf (stderr, "Read %d: %d\n", i, data_read[i]);
+      }
+    } else {
+      fprintf (stderr, "Error reading from dataset. reason: %d\n", rc);
+    }
 
-  rc = hio_read (element, 1028, 0, data_read, 5, sizeof (int));
-  if (0 < rc) {
-    for (int i = 0 ; i < rc/sizeof(int) ; ++i) {
-      fprintf (stderr, "Read %d: %d\n", i, data_read[i]);
+    rc = hio_close (&element);
+    if (HIO_SUCCESS != rc) {
+      fprintf (stderr, "Error closing element. reason: %d\n", rc);
     }
   } else {
-    fprintf (stderr, "Error reading from dataset. reason: %d\n", rc);
-  }
-
-  rc = hio_close (&element);
-  if (HIO_SUCCESS != rc) {
-    fprintf (stderr, "Error closing element. reason: %d\n", rc);
+      fprintf (stderr, "Could not create dataset element. reason: %d\n", rc);
   }
 
   rc = hio_dataset_close (&dataset);
