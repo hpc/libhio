a64290ac9397ae525ce5fe70992dd12faef2c92d
Nathan Hjelm <hjelmn@lanl.gov>
2015-05-05 10:54:55 -0600

    Add missing file
    Signed-off-by: Nathan Hjelm <hjelmn@lanl.gov>

---
+++ src/builtin-posix_component.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C; c-basic-offset:2 ; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2014-2015 Los Alamos National Security, LLC.  All rights
+ *                         reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#if !defined(BUILTIN_POSIX_COMPONENT_H)
+#define BUILTIN_POSIX_COMPONENT_H
+
+#include "hio_types.h"
+#include "hio_component.h"
+#include "hio_module.h"
+
+/* data types */
+typedef struct builtin_posix_module_t {
+  hio_module_t base;
+  mode_t access_mode;
+} builtin_posix_module_t;
+
+typedef struct builtin_posix_module_dataset_t {
+  struct hio_dataset_t base;
+  pthread_mutex_t      lock;
+  FILE                *fh;
+  uint64_t             bytes_read;
+  uint64_t             bytes_written;
+  char                *base_path;
+} builtin_posix_module_dataset_t;
+
+extern hio_component_t builtin_posix_component;
+
+#endif /* BUILTIN_POSIX_COMPONENT_H */
