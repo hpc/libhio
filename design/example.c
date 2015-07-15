279489d026a255baff04644ed977a97dcda96e34
Nathan Hjelm <hjelmn@lanl.gov>
2015-07-15 13:48:12 -0600

    Update documentation example
    Signed-off-by: Nathan Hjelm <hjelmn@lanl.gov>

--- design/example.c
+++ design/example.c
@@ -29,12 +29,14 @@ int initialize_hio (MPI_Comm comm) {
   MPI_Comm_size (comm, &nranks);
 
   /* initialize the hio state using mpi using the file input.deck for configuration */
-  hrc = hio_init_mpi (&hio_context, &comm, "input.deck", "#HIO", "CFL");
+  hrc = hio_init_mpi (&hio_context, &comm, "input.deck", "#HIO", "example_context");
   if (HIO_SUCCESS != hrc) {
     /* print out the error */
     (void) hio_err_print_all (hio_context, args);
     return -1;
   }
+
+  /* add data will appear in example_context.hio */
 
   return 0;
 }
@@ -52,6 +54,7 @@ void checkpoint (void *data, size_t size, int timestep) {
   hio_element_t hio_element;
   hio_dataset_t hio_set;
   hio_request_t hio_requests[3], master_request;
+  size_t bytes_transferred[3];
   hio_return_t hrc;
   int hint;
 
@@ -71,41 +74,41 @@ void checkpoint (void *data, size_t size, int timestep) {
   }
 
   /* Set the stage mode on the dataset */
-  hrc = hio_config_set_value (hio_set, "dataset_stage_mode", "lazy");
+  hrc = hio_config_set_value (hio_set, "datawarp_stage_mode", "lazy");
   if (HIO_SUCCESS != rc) {
     /* print out the error */
     (void) hio_err_print_last (hio_context, "Could not set stage mode configuration on HIO n-n dataset");
   }
 
-  hrc = hio_config_set_value (hio_set, "dataset_stripe_width", "4096");
+  hrc = hio_config_set_value (hio_set, "stripe_width", "4096");
   if (HIO_SUCCESS != rc) {
     /* print out the error */
     (void) hio_err_print_last (hio_context, "Could not set stripe width configuration on HIO dataset");
   }
 
-  hrc = hio_open (hio_set, &hio_element, "data", HIO_FLAG_CREAT | HIO_FLAG_EXCLUSIVE | HIO_FLAG_WRITE, 0644);
+  hrc = hio_element_open (hio_set, &hio_element, "data", HIO_FLAG_CREAT | HIO_FLAG_EXCLUSIVE | HIO_FLAG_WRITE, 0644);
   if (HIO_SUCCESS != rc) {
     PRINT_AND_EXIT("Could not open element \"data\" for writing");
   }
 
   offset = 0;
-  hrc = hio_write (hio_element, offset, &size, sizeof (size_t));
+  hrc = hio_element_write (hio_element, offset, &size, sizeof (size));
   if (hrc > 0) {
     offset += hrc;
   }
 
-  hrc = hio_write_strided (hio_element, offset, data, size, sizeof (float), 16);
+  hrc = hio_element_write_strided (hio_element, offset, data, size, sizeof (float), 16);
   if (hrc > 0) {
     offset += hrc;
   }
 
-  hrc = hio_write_strided (hio_element, offset, (void *)((intptr_t) data +
+  hrc = hio_element_write_strided (hio_element, offset, (void *)((intptr_t) data +
                            sizeof (float)), size, sizeof (int), 16);
   if (hrc > 0) {
     offset += hrc;
   }
   /* flush all data locally (not really needed here since the close will flush everything) */
-  hrc = hio_flush (hio_element, HIO_FLUSH_MODE_LOCAL);
+  hrc = hio_element_flush (hio_element, HIO_FLUSH_MODE_LOCAL);
   if (HIO_SUCCESS != hrc) {
     PRINT_AND_EXIT("Error writing to HIO file");
   }
@@ -116,6 +119,7 @@ void checkpoint (void *data, size_t size, int timestep) {
     PRINT_AND_EXIT("Error when closing HIO file");
   }
 
+  /* collective call finalizing the dataset */
   hrc = hio_dataset_close (&hio_set);
   if (HIO_SUCCESS != rc) {
     PRINT_AND_EXIT("Error when closing n-n HIO dataset");
@@ -132,31 +136,32 @@ void checkpoint (void *data, size_t size, int timestep) {
   }
 
   /* example of writing out a shared file */
-  hrc = hio_open (hio_set, &hio_element, "shared", HIO_FLAG_CREAT | HIO_FLAG_SHARED | HIO_FLAG_WRITE, 0644);
+  hrc = hio_element_open (hio_set, &hio_element, "shared", HIO_FLAG_CREAT | HIO_FLAG_SHARED | HIO_FLAG_WRITE, 0644);
   if (HIO_SUCCESS != rc) {
     PRINT_AND_EXIT("Could not open element \"shared\" for writing");
   }
 
   offset = my_rank * sizeof (size_t);
-  hrc = hio_write_nb (hio_element, hio_requests, offset, &size, sizeof (size_t));
+  hrc = hio_element_write_nb (hio_element, hio_requests, offset, &size, sizeof (size_t));
 
   offset = nranks * sizeof (size_t) + size * sizeof (float) * my_rank;
-  hrc = hio_write_strided_nb (hio_element, hio_requests + 1, offset, data, size,
+  hrc = hio_element_write_strided_nb (hio_element, hio_requests + 1, offset, data, size,
                               sizeof (float), 16);
 
   offset = nranks * (sizeof (size_t) + size * sizeof (float)) + my_rank * size * sizeof (int);
-  hrc = hio_write_strided_nb (hio_element, hio_requests + 2, offset, (void *)((intptr_t) data +
+  hrc = hio_element_write_strided_nb (hio_element, hio_requests + 2, offset, (void *)((intptr_t) data +
                               sizeof (float)), size, sizeof (int), 16);
 
   hrc = hio_request_join (hio_requests, 3, &master_request);
 
-  hrc = hio_wait (&master_request);
+  hrc = hio_request_wait (hio_requests, 3, &bytes_transferred);
 
-  hrc = hio_close (&hio_element);
+  hrc = hio_element_close (&hio_element);
   if (HIO_SUCCESS != rc) {
     PRINT_AND_EXIT("Error encountered when closing HIO element");
   }
 
+  /* collective call finalizing the dataset */
   hrc = hio_set_close (&hio_set);
   if (HIO_SUCCESS != rc) {
     (void) hio_err_print_last (hio_context, "Error encountered when closing HIO dataset");
