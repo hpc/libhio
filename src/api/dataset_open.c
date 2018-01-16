2f95e01601dbbefb3ab11ee9da32cb674fc6ee09
Nathan Hjelm <hjelmn@lanl.gov>
2018-01-16 14:16:01 -0700

    core: ensure we check for an existing dataset before importing
    Signed-off-by: Nathan Hjelm <hjelmn@lanl.gov>

--- src/api/dataset_open.c
+++ src/api/dataset_open.c
@@ -13,48 +13,6 @@
 
 #include <stdlib.h>
 
-static int hioi_dataset_open_last (hio_dataset_t dataset) {
-  hio_context_t context = hioi_object_context ((hio_object_t) dataset);
-  hio_dataset_list_t *list;
-  int64_t id = dataset->ds_id;
-  hio_module_t *module;
-  int rc = HIO_ERR_NOT_FOUND;
-
-  list = hioi_dataset_list_get (context, hioi_object_identifier (dataset), id);
-  if (NULL == list) {
-    return HIO_ERR_OUT_OF_RESOURCE;
-  }
-
-  for (int i = list->header_count - 1 ; i >= 0 ; --i) {
-    module = list->headers[i].module;
-
-    if (0 != list->headers[i].ds_status) {
-      hioi_log (context, HIO_VERBOSE_DEBUG_MED, "skipping dataset with non-zero status: %s::%" PRId64
-                ". status = %d", hioi_object_identifier (&dataset->ds_object), list->headers[i].ds_id,
-                list->headers[i].ds_status);
-      continue;
-    }
-
-    hioi_log (context, HIO_VERBOSE_DEBUG_MED, "attempting to open dataset %s::%" PRId64 " on data root "
-              "%s (module: %p). index %d", hioi_object_identifier (&dataset->ds_object),
-              list->headers[i].ds_id, module->data_root, module, i);
-
-    /* set the current dataset id to the one we are attempting to open */
-    dataset->ds_id = list->headers[i].ds_id;
-    rc = hioi_dataset_open_internal (module, dataset);
-    if (HIO_SUCCESS == rc) {
-      break;
-    }
-
-    /* reset the id to the id originally requested */
-    dataset->ds_id = id;
-  }
-
-  hioi_dataset_list_release (list);
-
-  return rc;
-}
-
 static int hioi_dataset_open_specific (hio_context_t context, hio_dataset_t dataset) {
   int rc = HIO_ERR_NOT_FOUND;
 
@@ -77,7 +35,11 @@ static int hioi_dataset_open_specific (hio_context_t context, hio_dataset_t data
 }
 
 int hio_dataset_open (hio_dataset_t dataset) {
-  hio_context_t context;
+  hio_context_t context = hioi_object_context ((hio_object_t) dataset);
+  hio_dataset_list_t *list;
+  const int64_t id = dataset->ds_id;
+  hio_module_t *module;
+  int rc = HIO_ERR_NOT_FOUND;
 
   if (HIO_OBJECT_NULL == dataset) {
     return HIO_ERR_BAD_PARAM;
@@ -88,11 +50,45 @@ int hio_dataset_open (hio_dataset_t dataset) {
     dataset->ds_flags |= HIO_FLAG_CREAT;
   }
 
-  context = hioi_object_context ((hio_object_t) dataset);
-
-  if (HIO_DATASET_ID_HIGHEST == dataset->ds_id || HIO_DATASET_ID_NEWEST == dataset->ds_id) {
-    return hioi_dataset_open_last (dataset);
+  list = hioi_dataset_list_get (context, hioi_object_identifier (dataset), id);
+  if (NULL == list) {
+    return HIO_ERR_OUT_OF_RESOURCE;
   }
 
-  return hioi_dataset_open_specific (context, dataset);
+  for (int i = list->header_count - 1 ; i >= 0 ; --i) {
+    module = list->headers[i].module;
+
+    if (0 != list->headers[i].ds_status) {
+      hioi_log (context, HIO_VERBOSE_DEBUG_MED, "skipping dataset with non-zero status: %s::%" PRId64
+                ". status = %d", hioi_object_identifier (&dataset->ds_object), list->headers[i].ds_id,
+                list->headers[i].ds_status);
+      continue;
+    }
+
+    hioi_log (context, HIO_VERBOSE_DEBUG_MED, "attempting to open dataset %s::%" PRId64 " on data root "
+              "%s (module: %p). index %d", hioi_object_identifier (&dataset->ds_object),
+              list->headers[i].ds_id, module->data_root, module, i);
+
+    if (HIO_DATASET_ID_HIGHEST == id || HIO_DATASET_ID_NEWEST == id || id == list->headers[i].ds_id) {
+      /* set the current dataset id to the one we are attempting to open */
+      dataset->ds_id = list->headers[i].ds_id;
+      rc = hioi_dataset_open_internal (module, dataset);
+      if (HIO_SUCCESS == rc) {
+        break;
+      }
+
+      /* reset the id to the id originally requested */
+      dataset->ds_id = id;
+    }
+  }
+
+  hioi_dataset_list_release (list);
+
+  if (HIO_SUCCESS != rc) {
+    /* If nothing was found just try to open the dataset. This is meant to support importing a POSIX
+     * file. At some point I plan to improve the logic so this isn't needed. */
+    return hioi_dataset_open_specific (context, dataset);
+  }
+
+  return rc;
 }
